41fb5553df5e932ff352e77d913018e81346f75b
JBeanCode <js02bean@gmail.com>
2026-01-11 00:58:44 +0000

    joshs-complete-non-guided-excuse-generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,32 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+let who = ["My Grandpa","Batman","My chubby Aunt","George W. Bush", "The Rolling Stones", "Yoshi"]
+let action = ["ate","trampled","torched","crushed", "peed on", "ran off with"]
+let what = ["my phone","my TV","my car","my computer", "my house", "my girlfriend"]
+let when = ["when I was sleeping","when I was at McDonalds","when I was at the movies","when I was in class", "when I was 14"]
+
+function getRandomIndex(array){
+  return Math.floor(Math.random() * array.length)
+}
+  
+const whoValue = who[getRandomIndex(who)]
+const actionValue = action[getRandomIndex(action)]
+const whatValue = what[getRandomIndex(what)]
+const whenValue = when[getRandomIndex(when)]
+
+const entireExcuse = `${whoValue} ${actionValue} ${whatValue} ${whenValue}`
+
+console.log(entireExcuse)
+
+let excuseLine = this.document.querySelector("#excuse")
+  excuseLine.textContent = entireExcuse;
 };
+
+const myButton = document.getElementById("bigRedButton");
+
+function handleClick() {
+  location.reload(); // refreshes the page
+}
+myButton.addEventListener("click", handleClick);
+
